da301cf05baac7d5dba428365fddff58bacbf05b
Heeyoun Lim <heeyn.lim@gmail.com>
2025-01-17 09:54:37 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 104 ms, Memory: 14296 KB -BaekjoonHub

--- Java/백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
+++ Java/백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -1,19 +1,19 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-
-		for (int i = 0; i < N; i++) {
-			while (sc.hasNext()) {
-				StringBuilder sb = new StringBuilder(sc.next());
-				System.out.println(sb.reverse());
-			}
-		}
-		sc.close();
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder(); 
+        
+        // 두 정수 n행과 m열 중 n을 저장 
+        int n = Integer.parseInt(br.readLine().split(" ")[0]);
+        
+        // 배열뒤집기 반복문
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine(); 
+            sb.append(new StringBuilder(s).reverse().append("\n"));
+        }
+        System.out.println(sb);
+        br.close(); 
+    }
 }
